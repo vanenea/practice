ca39c2cfe2061bb32740d39e378f2ca197ef2324
vanenea <576700973@qq.com>
2018-05-28 20:47:30 +0800

    28.May.2018

--- src/main/java/com/chen/practice/Test1.java
+++ src/main/java/com/chen/practice/Test1.java
@@ -1,9 +1,22 @@
 package com.chen.practice;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Test1 {
 
     public static void main(String[] args) {
         System.out.printf("helloWorld!");
+        Collection<String> collection = add(new String[]{"string1","string2","string3"});
+        System.out.printf("collection:"+collection);
     }
 
+    public static Collection add(String[] strings) {
+        List<String> list = new ArrayList<>();
+        for(String s:strings){
+            list.add(s);
+        }
+        return list;
+    }
 }
