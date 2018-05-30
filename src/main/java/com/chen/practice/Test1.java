9cb8ab73fdacf54cdc09dd5636ce73c02c5531bf
vanenea <576700973@qq.com>
2018-05-30 22:43:01 +0800

    30.May.2018

--- src/main/java/com/chen/practice/Test1.java
+++ src/main/java/com/chen/practice/Test1.java
@@ -1,4 +1,5 @@
 package com.chen.practice;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -6,7 +7,24 @@ import java.util.List;
 public class Test1 {
 
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "root");
+            String sql = "select now() from dual";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet set = preparedStatement.executeQuery();
+            while(set.next()){
+                Date date = set.getDate(1);
+                System.out.println("date:"+date);
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
 
+        }
     }
 
     public static Collection add(String[] strings) {
