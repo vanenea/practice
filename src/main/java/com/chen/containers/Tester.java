8c148a41a21bab7180fdefdbcb848abbc7f6e747
vanenea <576700973@qq.com>
2018-07-14 23:51:58 +0800

    14.July.2018

--- src/main/java/com/chen/containers/Tester.java
+++ src/main/java/com/chen/containers/Tester.java
@@ -1,7 +1,6 @@
 package com.chen.containers;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,18 +10,33 @@ import java.util.List;
 public class Tester {
 
     public static void main(String[] args) {
-        Collection<String> list1 = new ArrayList<>();
+        List<String> list1 = new ArrayList<String>();
         list1.add("java");
         list1.add("c++");
         list1.add("go");
-
+        list1.add("java");
         List<String> list2 = new ArrayList<>();
         list2.add("java");
         list2.add("javaScript");
 
         System.out.println( Collections.disjoint(list1,list2));
-        System.out.println(list1.retainAll(list2));
+     //   System.out.println(list1.retainAll(list2));
         System.out.println(list1);
+
+        List<String> bs1 = new ArrayList<>();
+       // bs1.add("aaa");
+        bs1.add("bbb");
+        bs1.add("ccc");
+        bs1.add("ddd");
+        bs1.add("aaa");
+
+
+        System.out.println(list1.size());
+        String key = list1.get(0);
+        //使用二分法查找元素时，必须先排序
+        Collections.sort(bs1);
+        System.out.println(bs1);
+        System.out.println(Collections.binarySearch(bs1,"aaa"));
 
     }
 }
