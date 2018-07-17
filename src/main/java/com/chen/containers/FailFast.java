94daad2664aeeb1a6d4475d3bb7cb68818943a78
vanenea <576700973@qq.com>
2018-07-17 22:03:11 +0800

    17.July.2018

--- src/main/java/com/chen/containers/FailFast.java
+++ src/main/java/com/chen/containers/FailFast.java
@@ -1,18 +1,19 @@
 package com.chen.containers;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 /**
  * 快速修改异常
  */
 public class FailFast {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        Iterator<String> it = list.iterator();
-        list.add("hello");
-        it.next();
+//        List<String> list = new ArrayList<>();
+//        Iterator<String> it = list.iterator();
+//        list.add("hello");
+//        it.next();
+        String str = "123,526,529";
+        System.out.println(str);
+        String[] arr = str.split(",");
+        System.out.println(arr.length);
+        System.out.println(arr);
     }
 }
