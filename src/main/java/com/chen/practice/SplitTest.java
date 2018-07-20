902d86d17fa9b408463c4453e8a9b8763fdc09a5
vanenea <576700973@qq.com>
2018-07-20 08:20:23 +0800

    20.July.2018

---
+++ src/main/java/com/chen/practice/SplitTest.java
@@ -0,0 +1,11 @@
+package com.chen.practice;
+
+public class SplitTest {
+
+	public static void main(String[] args) {
+		String str = "123";
+		String[] split = str.split(",");
+		
+		System.out.println(split.length+", "+split);
+	}
+}
