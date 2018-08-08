223038df5bfe73be448ff8e4d1e5d01ba904537b
vanenea <576700973@qq.com>
2018-08-08 22:02:22 +0800

    8.August.2018

---
+++ src/main/java/com/chen/jvm/RuntimeConstantPool.java
@@ -0,0 +1,16 @@
+package com.chen.jvm;
+
+public class RuntimeConstantPool {
+
+	public static void main(String[] args) {
+
+		String str1 = new StringBuilder("中国").append("钓鱼岛").toString();
+		System.out.println(str1.intern() == str1);
+
+		String str2 = new StringBuilder("ja").append("va").toString();
+		System.out.println(str2.intern() == str2);
+		
+		
+		
+	}
+}
