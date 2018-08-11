796fb968f60097153f82e2ef9924995e5ec1ff24
vanenea <576700973@qq.com>
2018-08-11 23:09:05 +0800

    11.August.2018

--- src/main/resources/static/js/angularJS.js
+++ src/main/resources/static/js/angularJS.js
@@ -1,10 +1,30 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $rootScope) {
-    $.ajax({
+app.controller('myCtrl', function($scope) {
+    $scope.users = [];
+	$.ajax({
     	url:'showUser',
     	type:'get',
     	typedata:'json',
     	success:function(result){
+    		var list = [];
+    		var obj;
+    		for(var i=0; i<12; i++){
+    			obj = {
+    				"list": i
+    			}
+    			list.push(obj);
+    		}
+    		
+    		for(var i=0; i<result.data.length; i++){
+    			var user = {
+    				"username":	result.data[i].username,
+    				"password": result.data[i].password
+    			}
+    			user.list = list
+    			$scope.users.push(user)
+    		}
+    		
+    		$scope.$apply();
     		console.log(result);
     	}
     })
