e09f7b882522158483933efc512b3db65c0775fa
vanenea <576700973@qq.com>
2018-09-05 19:03:38 +0800

    5.September.2018

--- src/main/java/com/chen/algorithms/Evaluate.java
+++ src/main/java/com/chen/algorithms/Evaluate.java
@@ -1,9 +1,20 @@
 package com.chen.algorithms;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Evaluate {
 
 	public static void main(String[] args) {
+		Stack<String> ops = new Stack<String>();
+		Scanner sca = new Scanner("");
+		while (sca.hasNext()) {
+			String str = sca.next();
+			if("(".equals(str)) {
+				
+			} else if ("+".equals(str)) {
+				
+			}
+		}
 	}
 }
