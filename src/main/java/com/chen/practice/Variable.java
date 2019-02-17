eabb8ba967cad142243d01743ffe8d976ee8ae6c
vanenea <576700973@qq.com>
2019-02-17 19:32:49 +0800

    17.February.2019

--- src/main/java/com/chen/practice/Variable.java
+++ src/main/java/com/chen/practice/Variable.java
@@ -1,43 +1,20 @@
 package com.chen.practice;
 
-import java.util.Calendar;
-import java.util.Date;
-
 public class Variable {
-
-	private int count = 9;
-
-	public static final double i = Math.random();
 	
 	public static void main(String[] args) {
-//		Variable va1 = new Variable();
-//		va1.count++;
-//		
-//		Variable va2 = new Variable();
-//		va2.count--;
-//		System.out.println("count:"+va1.count);
-//		System.out.println("count:"+va2.count);
-		System.out.println(i);
 		
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DAY_OF_YEAR, 1);
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		Date d = new Date();
-		long time = calendar.getTimeInMillis()-d.getTime();
-		System.out.println(time);
-		double hours =  time/(1000.00*60*60);
-		System.out.println(hours);
-		
-		String temp = "helloWold";
-		assert temp.length()==2;
-		System.out.println("123213");
+		System.out.println("111111");
+		try {
+			test();
+		} catch (Exception e) {
+			System.out.println("22222");
+			throw new RuntimeException("出错了");
+		}
+		System.out.println("333333");
 	}
 	
-	class demo{
-		public void prin() {
-			System.out.println(count);
-		}
+	public static void test() {
+		throw new RuntimeException("出错了");
 	}
 }
