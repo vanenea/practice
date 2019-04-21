60a872b84a1e270d90e5f498693f84122c89cd4b
vanenea <576700973@qq.com>
2019-04-21 21:58:54 +0800

    21.April.2019

--- src/main/java/com/chen/practice/DoSomething.java
+++ src/main/java/com/chen/practice/DoSomething.java
@@ -1,43 +1,18 @@
 package com.chen.practice;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
 public class DoSomething {
 
-	public enum Company{
-		BOSS{void action(){System.out.println("this is boss");}};
-		abstract void action();
+	public static void main(String[] args) {
+		int a = 5 & 1;
+		System.out.println(a);
+		System.out.println(-5/3);
+		int b = 3;
+		add(b);
+		System.out.println(b);
+		
 	}
 	
-	public static void main(String[] args) {
-		Company[] company = Company.values();
-		for (Company company2 : company) {
-			company2.action();
-		}
-		Pattern pa = Pattern.compile("\\d+");
-		System.out.println(pa.matcher("52").matches());
-		System.out.println(Float.isNaN(Integer.parseInt("123")));
-		
-		
-		
-		
-//		String str = null;
-//		try {
-//			str.split(",");
-//			
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//			e.printStackTrace();
-//		}
-		
-		Map<String, String> m = new HashMap<>();
-		m.put("Chinese", "yellow");
-		m.put("American", "white");
-		System.out.println(m);
-		
-		String st = "he".intern();
-		System.out.println(st);
+	public static int add(int a) {
+		return ++a;
 	}
 }
