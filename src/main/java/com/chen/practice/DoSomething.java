55729333f211fa4e809091a592719be7a7fa47e1
vanenea <576700973@qq.com>
2019-04-27 21:56:29 +0800

    27.April.2019

--- src/main/java/com/chen/practice/DoSomething.java
+++ src/main/java/com/chen/practice/DoSomething.java
@@ -4,7 +4,9 @@ public class DoSomething {
 
 	public static void main(String[] args) {
 		int a = 5 & 1;
+		int c = 3 | 2;
 		System.out.println(a);
+		System.out.println(c);
 		System.out.println(-5/3);
 		int b = 3;
 		add(b);
