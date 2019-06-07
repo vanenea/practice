f3333a6a64baf8d97b455d18f4f71cc43d60ec63
vanenea <576700973@qq.com>
2019-06-07 22:45:35 +0800

    7.June.2019

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -1,6 +1,11 @@
 package com.chen.practice;
 
-import org.apache.commons.collections.comparators.ReverseComparator;
+import static org.assertj.core.api.Assertions.in;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.ss.formula.functions.Roman;
 
 /**
  * leetcode算法题
@@ -36,6 +41,7 @@ public class Leetcode {
     }
     
    /**
+    * 回文
     *  Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 
 	    Example 1:
@@ -53,7 +59,6 @@ public class Leetcode {
 	    Output: false
 	    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
     */
-    
     public boolean isPalindrome(int x) {
     	if(x<0) return false;
     	char[] arry = String.valueOf(x).toCharArray();
@@ -63,8 +68,97 @@ public class Leetcode {
 		}
     	return true;
     }
+    
+    /**
+     * 罗马数字 </br>
+     * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M. <br>
+
+		Symbol       Value
+		I             1
+		V             5
+		X             10
+		L             50
+		C             100
+		D             500
+		M             1000
+		For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+		
+		Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+		
+		I can be placed before V (5) and X (10) to make 4 and 9. 
+		X can be placed before L (50) and C (100) to make 40 and 90. 
+		C can be placed before D (500) and M (1000) to make 400 and 900.
+		Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+     * @param s
+     * @return
+     */
+    public static int romanToInt(String s) {
+        Map<String, Integer> romanNumberals = new HashMap<String, Integer>();
+        romanNumberals.put("I", 1);
+        romanNumberals.put("V", 5);
+        romanNumberals.put("X", 10);
+        romanNumberals.put("L", 50);
+        romanNumberals.put("C", 100);
+        romanNumberals.put("D", 500);
+        romanNumberals.put("M", 1000);
+        
+        romanNumberals.put("IV", 4);
+        romanNumberals.put("IX", 9);
+        romanNumberals.put("XL", 40);
+        romanNumberals.put("XC", 90);
+        romanNumberals.put("CD", 400);
+        romanNumberals.put("CM", 900);
+        int sum = 0;
+        int index = -1;
+        do {
+        	if((index=s.indexOf("IV"))!=-1) {
+        		s = s.replace("IV", "");
+        		sum += 4;
+        	}
+        } while(index != -1);
+        do {
+        	if((index=s.indexOf("IX"))!=-1) {
+        		s = s.replace("IX", "");
+        		sum += 9;
+        	}
+        } while(index != -1);
+        do {
+        	if((index=s.indexOf("XL"))!=-1) {
+        		s = s.replace("XL", "");
+        		sum += 40;
+        	}
+        } while(index != -1);
+        do {
+        	if((index=s.indexOf("XC"))!=-1) {
+        		s = s.replace("XC", "");
+        		sum += 90;
+        	}
+        } while(index != -1);
+        do {
+        	if((index=s.indexOf("CD"))!=-1) {
+        		s = s.replace("CD", "");
+        		sum += 400;
+        	}
+        } while(index != -1);
+        do {
+        	if((index=s.indexOf("CM"))!=-1) {
+        		s = s.replace("CM", "");
+        		sum += 900;
+        	}
+        } while(index != -1);
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+        	sum += romanNumberals.get(String.valueOf(charArray[i]));
+		}
+        return sum;
+    }
+    
+    
+    
     public static void main(String[] args) {
 		System.out.println(reverse(-123));
+		System.out.println(romanToInt("MCMXCIV"));
+		
 	}
 	
 }
