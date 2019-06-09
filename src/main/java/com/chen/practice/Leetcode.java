99b34ea6edf85e4fd9d8e7578f0930bbecede0fd
vanenea <576700973@qq.com>
2019-06-09 21:06:51 +0800

    It's Sunday

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -154,11 +154,39 @@ public class Leetcode {
     }
     
     
+	/**
+	 * Write a function to find the longest common prefix string amongst an array of
+	 * strings.</br></br>
+	 * 
+	 * If there is no common prefix, return an empty string "".
+	 * <hr>
+	 * Example 1:
+	 * <code>
+	 * Input: ["flower","flow","flight"] Output: "fl" Example 2:
+	 * 
+	 * Input: ["dog","racecar","car"] Output: "" Explanation: There is no common
+	 * prefix among the input strings.
+	 *	</code>
+	 */
+    public static String longestCommonPrefix(String[] strs) {
+       if(strs.length==0) return "";
+       String prefix = strs[0];
+       for (int i = 1; i < strs.length; i++) {
+    	   while(strs[i].indexOf(prefix)!=0) {
+    		   prefix = prefix.substring(0, prefix.length()-1);
+    		   if(prefix.isEmpty())
+    			   return "";
+    	   }
+       }
+       return prefix;
+    }
+    
+    
     
     public static void main(String[] args) {
 		System.out.println(reverse(-123));
 		System.out.println(romanToInt("MCMXCIV"));
-		
+		System.out.println(longestCommonPrefix(new String[] {"flower", "flow", "flight"}));
 	}
 	
 }
