c91fc9a5e4808193cc244c9ca66c21f038180b6b
vanenea <576700973@qq.com>
2019-06-21 15:28:00 +0800

    21.June.2019

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -197,27 +197,36 @@ public class Leetcode {
 	 * valid.
 	 * </p>
 	 */
-	public boolean isValid(String s) {
-        Map<Character, Character> map = new HashMap<Character, Character>();
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
-        char[] charArray = s.toCharArray();
-        Stack<Character> re = new Stack<Character>();
-        for (int i = 0; i < charArray.length; i += 2) {
-        	Character current = charArray[i];
-        	Character next = charArray[i+1];
-        	if(!map.get(current).equals(next)) {
-        		re.add(current);
-        		re.add(next);
-        	} else {
-        		
-        	}
-        	
-		}
-		return false;
+    public boolean isValid(String s) {
+    	 // Hash table that takes care of the mappings.
+    	HashMap<Character, Character> mappings = new HashMap<Character, Character>();;
+    	mappings.put(')', '(');
+        mappings.put('}', '{');
+        mappings.put(']', '[');
+        // Initialize a stack to be used in the algorithm.
+    	Stack<Character> stack = new Stack<Character>();
+  		for (int i = 0; i < s.length(); i++) {
+  			char c = s.charAt(i);
+  			// If the current character is a closing bracket.
+  			if (mappings.containsKey(c)) {
+			// Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+  				char topElement = stack.empty() ? '#' : stack.pop();
+				// If the mapping for this bracket doesn't match the stack's top element, return false.
+				if (topElement != mappings.get(c)) {
+					return false;
+				}
+		} else {
+		  // If it was an opening bracket, push to the stack.
+		      stack.push(c);
+	    }
+	  }
+	
+	  // If the stack still contains elements, then it is an invalid expression.
+	  return stack.isEmpty();
     }
     
+	
+	
     public static void main(String[] args) {
 		System.out.println(reverse(-123));
 		System.out.println(romanToInt("MCMXCIV"));
