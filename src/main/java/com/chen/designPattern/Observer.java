1d2f6d6dbd76046eb47233070e8fcbeb5c42b886
vanenea <576700973@qq.com>
2019-07-03 17:34:58 +0800

    3.July.2019

---
+++ src/main/java/com/chen/designPattern/Observer.java
@@ -0,0 +1,6 @@
+package com.chen.designPattern;
+
+public interface Observer {
+
+	
+}
---
+++ src/main/java/com/chen/designPattern/Subject.java
@@ -0,0 +1,11 @@
+package com.chen.designPattern;
+
+public class Subject {
+
+	public Subject() {
+		java.util.Observer ob;
+		java.util.Observable oba;
+	}
+	
+	
+}
