42ba73bdbdfe618b2d281611a39592628cc97a25
vanenea <576700973@qq.com>
2019-11-20 19:45:08 +0800

    415615616

--- src/main/java/com/chen/designPattern/observer/Application.java
+++ src/main/java/com/chen/designPattern/observer/Application.java
@@ -4,9 +4,9 @@ public class Application {
 
 	public static void main(String[] args) {
 		NumberSubject subject = new NumberSubject();
-		BinaryObserver bo = new BinaryObserver(subject);
-		OctalObserver oo = new OctalObserver(subject);
-		HexObserver ho = new HexObserver(subject);  
+		new BinaryObserver(subject);
+		new OctalObserver(subject);
+		new HexObserver(subject);  
 		subject.setState(20);
 	}
 }
