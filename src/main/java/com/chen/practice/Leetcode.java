2b1ca256ece9d90ac5dd751227b08c6f698fd901
vanenea <576700973@qq.com>
2019-12-24 17:32:12 +0800

    1231231

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -253,7 +253,19 @@ public class Leetcode {
 	    }
 	    return i + 1;
     }
-	
+   
+    public int[] plusOne(int[] digits) {
+       for(int i=digits.length-1; i>=0; i--) {
+    	   if(digits[i] != 9) {
+    		   digits[i] ++ ;
+    		   return digits;
+    	   } else {
+    		   digits[i] = 0;
+    	   }
+       }
+       System.arraycopy(digits, 0, digits, 0, 1);
+    }
+    
     public static void main(String[] args) {
 		System.out.println(reverse(-123));
 		System.out.println(romanToInt("MCMXCIV"));
