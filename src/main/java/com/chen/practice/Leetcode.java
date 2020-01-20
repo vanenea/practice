b1c7b4f65b648af1278a441ac3a1610cdde37120
vanenea <576700973@qq.com>
2020-01-20 17:28:35 +0800

    January.20.2020

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -29,14 +29,54 @@ public class Leetcode {
 		System.out.println(singleNumber(new int[] {10,186,-49,176,118,167,-61,189,6,-24,7,-93,71,112,187,45,-36,38,82,108,-46,112,51,165,-37,159,1,-53,7,38,90,181,-23,91,-42,172,-95,130,84,149,-47,68,126,-67,175,22,121,131,84,114,60,64,182,-75,-17,-71,69,-92,103,-91,-91,86,126,166,33,-36,-80,-37,118,-80,-18,127,36,-71,-82,-82,144,12,57,149,71,91,-83,-100,-30,45,186,16,-51,-72,-83,107,140,-97,-93,1,12,189,-61,-50,180,98,96,-29,193,167,57,-45,16,6,-76,4,109,-23,22,144,190,-97,193,-51,-99,-79,-47,142,107,175,109,121,190,90,34,32,63,-24,41,-53,41,89,130,-18,-99,103,86,127,-30,102,32,-49,181,-60,114,60,-29,182,-75,168,96,51,33,142,108,69,10,-57,166,48,82,161,-17,-50,102,63,-45,140,180,176,-95,36,-46,168,187,161,-72,-100,-42,165,-76,-67,89,159,64,34,98,4,-60,172,-79,68,48,131,-57}));
 	}
 	
-	
+	/**
+	 * Count Primes
+	 * Count the number of prime numbers less than a non-negative number, n.
+	 * @param n
+	 * @return
+	 */
+	public int countPrimes(int n) {
+		int number = 2;
+		int a = 0;
+		if(n>=number) {
+			
+			for(int i = 1; i < number; i++) {
+				number /
+				
+			}
+		}
+		n
+    }
+	/**
+	 * happy number
+	 * @param n
+	 * @return
+	 */
+	public boolean isHappy(int n) {
+		Set<Integer> set = new HashSet<Integer>();
+		int squaer;
+		while(set.add(n)) {
+			squaer = 0;
+			do {
+				int a = n%10;
+				squaer += (a*a);
+				n = n/10;
+			} while (n>0);
+			if(squaer==1) 
+				return true;
+			else
+				n = squaer;
+		}
+		return false;
+	}
+	 
 	public static int singleNumber(int[] nums) {
-		String con = "";
+		StringBuilder con = new StringBuilder();
 		for(int i = 0; i < nums.length-1; i++ ) {
 			if(con.indexOf("#"+String.valueOf(i) + "#")!=-1) continue;
 			for(int j = i+1; j < nums.length; j++) {
 				if(nums[i] == nums[j]) {
-					con += "#"+ j + "#";
+					con.append("#"+ j + "#");
 					break;
 				}
 				if(j==nums.length-1) return nums[i];
