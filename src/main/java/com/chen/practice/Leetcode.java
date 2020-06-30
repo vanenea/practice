2c4bba577402f81156bf65a6d4fa4e0b366b65e0
vanenea <576700973@qq.com>
2020-06-30 17:22:47 +0800

    June.30.2020

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -1,12 +1,6 @@
 package com.chen.practice;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * LeetCode算法题
@@ -14,18 +8,44 @@ import java.util.Stack;
  * @author Chen.Y
  */
 public class Leetcode {
-    static Map<Integer, Long>  param = new HashMap<>();
+    static Map<Integer, Long> param = new HashMap<>();
 
     public static void main(String[] args) {
-        long begin = System.currentTimeMillis();
-       // System.out.println(fib(1000));
-        long current = System.currentTimeMillis();
-        System.out.println("Time:" + (current - begin) / 1000);
+        permute(new int[]{1,2,3});
+        System.out.println(res);
+    }
 
-        long begin1 = System.currentTimeMillis();
-        System.out.println(fibBWL(1000));
-        long current1 = System.currentTimeMillis();
-        System.out.println("Time1:" + (current1 - begin1) / 1000);
+    static List<List<Integer>> res = new LinkedList<>();
+
+    /* 主函数，输入一组不重复的数字，返回它们的全排列 */
+    public static List<List<Integer>> permute(int[] nums) {
+        // 记录「路径」
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, track);
+        return res;
+    }
+
+    // 路径：记录在 track 中
+    // 选择列表：nums 中不存在于 track 的那些元素
+    // 结束条件：nums 中的元素全都在 track 中出现
+    public static void backtrack(int[] nums, LinkedList<Integer> track) {
+        // 触发结束条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            // 排除不合法的选择
+            if (track.contains(nums[i]))
+                continue;
+            // 做选择
+            track.add(nums[i]);
+            // 进入下一层决策树
+            backtrack(nums, track);
+            // 取消选择
+            track.removeLast();
+        }
     }
 
     public static long fibBWL(int n) {
