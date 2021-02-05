cc0ee96c9d953b90c0391f68415079b0198b07c9
vanenea <576700973@qq,com>
2021-02-05 20:54:04 +0800

    28.January.2021

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -11,7 +11,97 @@ public class Leetcode {
     static Map<Integer, Long> param = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println(mySqrt(5));
+        merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6},3);
+
+    }
+
+    /**
+     * 88. Merge Sorted Array
+     * Easy
+     * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+     * <p>
+     * The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+     * Example 1:
+     * <p>
+     * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * Output: [1,2,2,3,5,6]
+     * Example 2:
+     * <p>
+     * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+     * Output: [1]
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        List<Integer> list = new ArrayList<>();
+
+        int i = 0, j = 0;
+        while (true) {
+            if (i == m || j == n) {
+                break;
+            }
+            if (nums1[i] < nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                list.add(nums2[j]);
+                j++;
+            } else {
+                list.add(nums1[i]);
+                list.add(nums2[j]);
+                i++;
+                j++;
+            }
+        }
+        if (i < m) {
+            for (; i < m; i++) {
+                list.add(nums1[1]);
+            }
+            for (; j < n; j++) {
+                list.add(nums2[j]);
+            }
+        }
+        nums1 = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            nums1[k] = list.get(k);
+        }
+    }
+
+    /**
+     * 888. 公平的糖果棒交换
+     * 爱丽丝和鲍勃有不同大小的糖果棒：A[i] 是爱丽丝拥有的第 i 根糖果棒的大小，B[j] 是鲍勃拥有的第 j 根糖果棒的大小。
+     * <p>
+     * 因为他们是朋友，所以他们想交换一根糖果棒，这样交换后，他们都有相同的糖果总量。（一个人拥有的糖果总量是他们拥有的糖果棒大小的总和。）
+     * <p>
+     * 返回一个整数数组 ans，其中 ans[0] 是爱丽丝必须交换的糖果棒的大小，ans[1] 是 Bob 必须交换的糖果棒的大小。
+     * <p>
+     * 如果有多个答案，你可以返回其中任何一个。保证答案存在。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：A = [1,1], B = [2,2]
+     * 输出：[1,2]
+     * 示例 2：
+     * <p>
+     * 输入：A = [1,2], B = [2,3]
+     * 输出：[1,2]
+     * 示例 3：
+     * <p>
+     * 输入：A = [2], B = [1,3]
+     * 输出：[2,3]
+     * 示例 4：
+     * <p>
+     * 输入：A = [1,2,5], B = [2,4]
+     * 输出：[5,4]
+     */
+    public static int[] fairCandySwap(int[] A, int[] B) {
+        int cz = Arrays.stream(A).sum() - Arrays.stream(B).sum();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] - B[j] == cz / 2) {
+                    return new int[]{A[i], B[j]};
+                }
+            }
+        }
+        return null;
     }
 
     /**
@@ -57,7 +147,7 @@ public class Leetcode {
         int total = Arrays.stream(nums).sum();
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            if( 2*sum + nums[i] == total){
+            if (2 * sum + nums[i] == total) {
                 return i;
             }
             sum += nums[i];
