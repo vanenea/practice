3c44a864ae83f2d0f11b1b3f9e70a9d0e43ecc94
vanenea <576700973@qq.com>
2021-04-08 18:51:04 +0800

    8.April.2021

--- src/main/java/com/chen/practice/Leetcode.java
+++ src/main/java/com/chen/practice/Leetcode.java
@@ -172,6 +172,7 @@ public class Leetcode {
      * Input: 8
      * Output: 2
      * Explanation: The square root of 8 is 2.82842..., and since
+     * .+
      * the decimal part is truncated, 2 is returned.
      */
     public static int mySqrt(int x) {
