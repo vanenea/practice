d9aac6b646089963b11d99845a85924d6937a8b5
vanenea <576700973@qq.com>
2021-07-17 17:15:56 +0800

    July,17,2021

--- src/main/java/com/chen/jvm/StringTable.java
+++ src/main/java/com/chen/jvm/StringTable.java
@@ -13,13 +13,23 @@ public class StringTable {
         String s6 = s4.intern();
 
         //问
-        System.out.println(s3 == s4);   // ture
+        System.out.println(s3 == s4);   // false
         System.out.println(s3 == s5);   //true
         System.out.println(s3 == s6);   //true
-        String x2 = new String("c") + new String("d");
-        String x1 = "cd";
-        x2.intern();
 
+
+        String x2 = new String("c") + new String("d");
+
+        String x3 = x2.intern();
+        System.out.println(x2 == "cd"); //true    1.6   false
+        System.out.println(x3 == "cd"); //true    1.6   true
+
+        String x11 = "cd";
+        String x22 = new String("c") + new String("d");
+
+        String x33 = x22.intern();
+        System.out.println(x33 == x11);  //true
+        System.out.println(x22 == x11);     //false
 
         //问：如果调换了【最后两行代码】的位置呢 ，如果是jdk1.6呢
 
