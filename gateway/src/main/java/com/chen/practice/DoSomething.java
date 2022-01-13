fca8ef7c6ddd4cfd88b4439a4084f317bb3356e3
vanenea <576700973@qq.com>
2022-01-13 12:23:34 +0800

    26.September.2021

--- gateway/src/main/java/com/chen/practice/DoSomething.java
+++ gateway/src/main/java/com/chen/practice/DoSomething.java
@@ -1,9 +1,21 @@
 package com.chen.practice;
 
+import com.chen.domain.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.function.Function;
+
 public class DoSomething {
 
-	public static void main(String[] args) {
 
-	}
-	
+    @Test
+	public void testOptional(){
+	    User user = new User();
+	    user.setUsername("Chen");
+	    user.setPassword("123");
+       // Optional<User> u = Optional.of(user);
+        String u = Optional.ofNullable(user).map(user1 -> user1.getUsername()+user1.getPassword()).orElse("default");
+        System.out.println(u);
+    }
 }
